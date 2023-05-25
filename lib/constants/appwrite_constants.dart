e64974939fd4e527922a0c58aa715e513600ee7b
zagkun2k <zagkun2k@gmail.com>
2023-05-25 21:29:20 +0700

    change ipconfig v4

--- lib/constants/appwrite_constants.dart
+++ lib/constants/appwrite_constants.dart
@@ -1,7 +1,7 @@
 class AppwriteConstants {
   static const String databaseId = '6443e920c18eefda36dd';
   static const String projectId = '6443e4fd40cd53b65bed';
-  static const String endPoint = 'http://192.168.1.9/v1';
+  static const String endPoint = 'http://192.168.1.4/v1';
   //http://IPV4_USER/v1
 
   static const String usersCollection = '6443ee9ad69ca8ac1c33';
@@ -11,6 +11,6 @@ class AppwriteConstants {
   static const String storageBucket = '6443eeca614a0c048a54';
 
   static String imageUrl(String imageId) {
-    return '$endPoint/storage/buckets/$storageBucket/files/$imageId/view?project=$projectId&mode=addmin';
+    return '$endPoint/storage/buckets/$storageBucket/files/$imageId/view?project=$projectId&mode=admin';
   }
 }
